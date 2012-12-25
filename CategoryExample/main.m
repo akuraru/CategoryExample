653329119f8d21b82430971b0bc6d6d719511789
akuraru <akuraru+bb@gmail.com>
2012-12-25 18:41:45 +0900

    Initial Commit

---
+++ CategoryExample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CategoryExample
+//
+//  Created by P.I.akura on 2012/12/25.
+//  Copyright (c) 2012年 P.I.akura. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
